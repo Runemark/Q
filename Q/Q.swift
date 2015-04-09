fd49389dab3ec4e97b113e7744135a2952eb9041
Runemark <martin@runemark.com>
2015-04-09 12:36:46 -0600

    Initial testing

---
+++ Q/Q.swift
@@ -0,0 +1,21 @@
+//
+//  Q.swift
+//  Q
+//
+//  Created by Alicia Cicon on 4/9/15.
+//  Copyright (c) 2015 Runemark. All rights reserved.
+//
+
+import Foundation
+
+class Q
+{
+    var states:Int
+    var actions:Int
+    
+    init(states:Int, actions:Int)
+    {
+        self.states = states
+        self.actions = actions
+    }
+}
